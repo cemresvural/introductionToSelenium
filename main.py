ef3146786bd3f7efba397a84d0d8b22ad3776019
cemresvural <cemresvural@gmail.com>
2025-01-22 20:16:46 +0300

    find elements(better approach)

--- main.py
+++ main.py
@@ -2,17 +2,22 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 
 import time
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
+
 
 
 driver=webdriver.Chrome()
 driver.get("https://www.miuul.com")
-
+time.sleep(2)
 h1_elem=driver.find_element(By.XPATH,"//h1")
 a_elem=driver.find_element(By.XPATH,"//a")
 p_elem=driver.find_element(By.XPATH,"//p")
 
-selector=(By.XPATH,"//p")
-wait=WebDriverWait(driver,10)
-p_element=wait.until(EC.visibility_of_element_located(selector))
+p_elements=driver.find_elements(By.XPATH,"//p")
+p_elements
+elem=None
+if p_elements:
+    elem=p_elements[0]
+else:
+    print("Element not found")
+
+print(elem)

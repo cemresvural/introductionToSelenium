a7025682f9f81f4996ebb41b29e5763df3b4954c
cemresvural <cemresvural@gmail.com>
2025-02-16 21:29:31 +0300

    using proxy with selenium

--- main.py
+++ main.py
@@ -1,28 +1,17 @@
+import requests
 from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-import time
+from extension import proxies
 
-import pandas as pd
+url='https://www.ıpaddress.my/'
 
-# Initialize Driver
-options=webdriver.ChromeOptions()
-options.add_argument("--start-maximized")
-driver=webdriver.Chrome(options)
-driver.get("https://miuul.com/katalog")
-time.sleep(2)
+proxy_username =''
+proxy_password =''
+proxy_ip_address = ''
+proxy_port= ''
 
-select_option = driver.find_elements(By.XPATH, "//select[@name='sure']/option[contains(text(), '1-3 ay')]")
-select_option[0].click() if select_option else None
+#username:password@ip_address:port
+options= webdriver.ChromeOptions()
+options.add_argument(f'--proxy-server={proxy_ip_address}:{proxy_port}')
 
-course_blocks=driver.find_elements(By.XPATH,"//div[contains(@class,'card catalog') and (contains(@class,'block'))]")
-
-for block in course_blocks:
-    course_title=block.find_elements(By.XPATH,".//h6")
-    course_desc=block.find_elements(By.XPATH,".//p")
-
-    course_title=course_title[0].get_attribute("innerText") if course_title else None
-    course_desc=course_desc[0].get_attribute("innerText") if course_desc else None
-
-    print(course_title)
-    print(course_desc)
+driver=webdriver.Chrome(options=options)
+driver.get(url)
